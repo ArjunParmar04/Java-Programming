ed50b38b9efbda71781b8218552a6d54fdee22e5
ArjunParmar04 <akparmar2108@gmail.com>
2022-09-08 11:37:39 +0530

    java practice files

--- IfElse.java
+++ IfElse.java
@@ -1,7 +1,28 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class IfElse {
     public static void main(String[] args) {
+//       salary();
+//       oddOrEven();
+        leapYear();
+
+
+    }
+
+    static void oddOrEven(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int num = sc.nextInt();
+
+        if(num % 2 == 0){
+            System.out.println("Number is Even..");
+        }else {
+            System.out.println("Number is Odd..");
+        }
+    }
+
+
+    static void salary(){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Salary : ");
         int salary = sc.nextInt();
@@ -12,5 +33,25 @@ public class IfElse {
             salary += 1000;
         }
         System.out.println("Your Salary with bonus is : " + salary);
+    }
+    
+     static void leapYear(){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Year : ");
+        int year = sc.nextInt();
+
+        if (year % 4 == 0){
+            if (year % 100 == 0){
+                if (year % 400 == 0) {
+                    System.out.println("It's a leap year");
+                }
+            }else {
+                System.out.println("It's a leap year");
+            }
+        }
+        else {
+            System.out.println("It's not a leap year..");
+        }
     }
 }
