5d7ef115d4a65a6809d5c6c229bf38dfac16a1d2
ArjunParmar04 <akparmar2108@gmail.com>
2022-09-09 18:42:12 +0530

    java programming

---
+++ Arrays.java
@@ -0,0 +1,13 @@
+public class Arrays {
+    public static void main(String[] args) {
+        //declaration..
+        int[] arr;//declaration of array. arr is getting defined in the stack
+        arr = new int[5];//Initialization.. //Actually here object is being created in the memory(heap)..
+
+
+
+        //direct initialization..
+        int[] arr1 = {1,2,3,4,5};
+
+    }
+}
