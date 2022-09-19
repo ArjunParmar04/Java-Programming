b083acd33e4f1155bd8ff55eade8a8afdb115373
ArjunParmar04 <akparmar2108@gmail.com>
2022-09-19 11:57:18 +0530

    JavaProgramming

--- Searching/Que2.java
+++ Searching/Que2.java
@@ -4,21 +4,21 @@ public class Que2 {
     public static void main(String[] args) {
 
     }
-    public int maximumWealth(int[][] accounts) {
+public int maximumWealth(int[][] accounts) {
 
-        int ans = Integer.MIN_VALUE;
-        for (int i = 0; i < accounts.length; i++) {
-            //when you start a new col, take a new sum for that row
-            int sum = 0;
-            for (int j = 0; j < accounts[i].length; j++) {
-                sum += accounts[i][j];
-            }
-
-            //now we have sum of accounts of person
-            if (sum > ans){
-                ans = sum;
-            }
+    int ans = Integer.MIN_VALUE;
+    for (int i = 0; i < accounts.length; i++) {
+        //when you start a new col, take a new sum for that row
+        int sum = 0;
+        for (int j = 0; j < accounts[i].length; j++) {
+            sum += accounts[i][j];
         }
-        return ans;
+
+        //now we have sum of accounts of person
+        if (sum > ans){
+            ans = sum;
+        }
     }
+    return ans;
+}
 }
